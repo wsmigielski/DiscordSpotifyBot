312362b1a1540e1c036ef9a68daa13f0bdd73e78
wsmigielski <wojciechsmigielski@outlook.com>
2017-04-25 19:16:29 +0100

    Added an option to stop the bot which also clears your playing status.

--- bot.js
+++ bot.js
@@ -6,33 +6,69 @@ var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
 var config = require('./config.json');
 var currentSong, newSong;
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.username}!`);
+// Script for exiting the process is from https://frankl.in/code/before-exit-scripts-in-node-js
+
+let preExit = [];
+
+// Catch exit
+process.stdin.resume();
+process.on('exit', code => {
+  let i;
+  console.log('Process exit');
+  for (i = 0; i < preExit.length; i++) {
+    preExit[i](code);
+  }
+  client.user.setGame();
+  process.exit(code);
 });
 
+// Catch CTRL+C
+process.on('SIGINT', () => {
+  console.log('\nCTRL+C...');
+  process.exit(0);
+});
+
+// Catch uncaught exception
+process.on('uncaughtException', err => {
+  console.dir(err, {
+    depth: null
+  });
+  process.exit(1);
+});
+
+// INSERT CODE
+console.log('Bot on, hit CTRL+C to exit :)');
+
+// Add pre-exit script
+preExit.push(code => {
+  console.log('Whoa! Exit code %d, cleaning up...', code);
+});
+
+
+
 spotify.getStatus(function(err, res) { //Initially sets your game to the current song
-    if (err) {
-        return console.error(err);
-    }
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Logged in as ${client.user.username}!`);
+  console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
 
-    console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
-
-    currentSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
-    client.user.setGame(currentSong);
+  currentSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
+  client.user.setGame(currentSong);
 });
 
 setInterval(function(err, res) { //This Updates the song every 10 seconds
-    spotify.getStatus(function(err, res) {
-        if (err) {
-            return console.error(err);
-        }
-        newSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
-        if (newSong != currentSong) {
-          client.user.setGame(newSong);
-          console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
-          currentSong = newSong;
-        }
-    });
+  spotify.getStatus(function(err, res) {
+    if (err) {
+      return console.error(err);
+    }
+    newSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
+    if (newSong != currentSong) {
+      client.user.setGame(newSong);
+      console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
+      currentSong = newSong;
+    }
+  });
 }, 10000); //Change the value here to change how often the song is updated
 
 client.login(config.token)
